7000cd25abeed0fa5533000ee82155b642de1442
Armilion <valentin.andre94@gmail.com>
2021-08-14 13:29:05 +0200

    Stack to Tab Navigator

--- App.js
+++ App.js
@@ -2,19 +2,35 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AudioList from './screens/AudioList';
+import Player from './screens/Player';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+/*screenOptions={({ route }) => ({
+        tabBarIcon: () => {
+          let iconName;
+          if (route.name === "AudioList")
+            iconName = "headphones"
+          else if (route.name === "Player")
+            iconName = "play";
+          return <FontAwesome5 name={iconName} size={24} color="purple" />
+        },
+        tabBarActiveTintColor: "purple",
+        tabBarInactiveTintColor: "gray"
+      })}*/
 
 export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="AudioList" component={AudioList} />
+        <Tab.Screen name="Player" component={Player} />
+      </Tab.Navigator>
+    </NavigationContainer >
 
   );
 }
